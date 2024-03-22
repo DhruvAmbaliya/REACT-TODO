22d332e176126a81def9c6ec021e606f37696cfe
DhruvAmbaliya <dhruvambaliya72@gmail.com>
2024-03-22 12:43:33 +0530

    test

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,12 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { Context } from '../main';
 import Loader from '../components/Loader.jsx';
 import { Navigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 
 const Profile=()=> {
   
   const {isAuthenticated,loading,user} = useContext(Context);
-  const [refresh,setRefresh] = useState(false);
-
-  useEffect(()=>{
-    setRefresh(prev=>!prev);
-  },[refresh]);
 
   // console.log(user);
   if(!isAuthenticated) return <Navigate to={"/login"} />

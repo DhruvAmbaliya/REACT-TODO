aee7507614cfd679c7a40b9e8d7c7c1c1b8e71cf
DhruvAmbaliya <dhruvambaliya72@gmail.com>
2024-03-28 12:47:12 +0530

    modify date

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import moment from 'moment'
 
 const TodoItem = ({title,description,isCompleted,updateHandler,deleteHandler,id,createdAt}) => {
   return (
@@ -7,13 +7,12 @@ const TodoItem = ({title,description,isCompleted,updateHandler,deleteHandler,id,
         <div className='flex flex-row'>
             <h4>Title : {title} </h4>
             <p className='ml-5'>Description : {description}  </p>
-            <p className='ml-5'>Time : {createdAt} </p>
-            {/* momentjs */} 
+            <p className='ml-5'>Time : {moment(createdAt).format('llll')} </p> 
         </div>
         <div className='flex flex-row'>
           <h4 className='ml-5'>Task Status : </h4>
           <input className='ml-1' onChange={()=>updateHandler(id)} type='checkbox' checked={isCompleted} />
-          <button className='ml-5 border border-2 border-slate-300 hover:bg-red-300 ' onClick={()=>deleteHandler(id)} >Delete</button>
+          <button className='ml-5 border-2 border-slate-300 hover:bg-red-300 ' onClick={()=>deleteHandler(id)} > Delete Task </button>
         </div>
     </div>
   )
